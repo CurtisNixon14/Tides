04c3eec33925e1988975903f2851e4758b525c2b
Curtis Nixon <curtis.nixon14@gmail.com>
2020-04-14 17:12:17 -0700

    Initial Commit

---
+++ Tides WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Tides WatchKit Extension
+//
+//  Created by Curtis NIXON on 4/11/20.
+//  Copyright © 2020 Curtis NIXON. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Tides/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tides
+//
+//  Created by Curtis NIXON on 4/11/20.
+//  Copyright © 2020 Curtis NIXON. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
